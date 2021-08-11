e86e1f4f5ae290c18b49adce746de75a3ffca278
Aditya Sethi <aditya.sethi@v2solutions.com>
2021-08-11 10:24:12 +0530

    shifting all functions in component

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,6 @@
 <template>
-  <TableData :columns="columns" :dataset="filteredData"></TableData>
-  <div class="page-change-section pull-left">
-    <div class="pull-left" v-if="currentEntry !== data.length">
-      <span class=" padding select-btn" @click="changePage('first')">&laquo;</span>
-      <span class=" padding select-btn" @click="changePage('previous')">&#8249;</span>
-      <span class=" padding text">Showing {{ currentPage }} of {{ pages }}</span>
-      <span class=" padding select-btn" @click="changePage('next')">&#8250;</span>
-      <span class=" padding select-btn" @click="changePage('last')">&raquo;</span>
-    </div>
-  </div>
-  <div class="select-entry-section pull-right">
-    <button
-      class="btn"
-      :class="{ selected: currentEntry === data.length }"
-      @click="updatePages(data.length)"
-    >
-      All
-    </button>
-    <button
-      class="btn"
-      v-for="(entry, i) in showEnteries"
-      :key="i"
-      :class="{ selected: currentEntry === entry }"
-      @click="updatePages(entry)"
-    >
-      {{ entry }}
-    </button>
+  <div v-if="data.length > 0">
+    <TableData :columns="columns" :dataset="data"></TableData>
   </div>
 </template>
 
@@ -66,53 +41,13 @@ export default {
       },
     ]);
     let data = ref([]);
-    let filteredData = ref([]);
-    let currentPage = ref(1);
-    let currentEntry = ref(10);
-    let pages = ref(0);
-    let showEnteries = ref([10, 25, 50, 100]);
-
+    
     axios.get("data.json").then((res) => {
       data.value = res.data;
-      pagination();
     });
-
-    let pagination = () => {
-      let trimStart = (currentPage.value - 1) * currentEntry.value;
-      let trimEnd = trimStart + currentEntry.value;
-      filteredData.value = data.value.slice(trimStart, trimEnd);
-      pages.value = Math.ceil(data.value.length / currentEntry.value);
-    };
-
-    let changePage = (page) => {
-      if (page === 'first') {
-        currentPage.value = 1;
-      } else if(page === 'previous') {
-        currentPage.value = currentPage.value === 1 ? currentPage.value : currentPage.value - 1; 
-      } else if (page === 'next') {
-        currentPage.value = currentPage.value === pages.value ? currentPage.value : currentPage.value + 1;
-      } else if (page === 'last') {
-        currentPage.value = pages.value;
-      }
-      pagination();
-    };
-
-    let updatePages = (entry) => {
-      currentEntry.value = entry;
-      currentPage.value = 1;
-      pagination();
-    };
 
     return {
       columns,
-      filteredData,
-      showEnteries,
-      currentEntry,
-      pagination,
-      pages,
-      changePage,
-      currentPage,
-      updatePages,
       data,
     };
   },
--- src/components/TableData.vue
+++ src/components/TableData.vue
@@ -6,18 +6,91 @@
       </tr>
     </thead>
     <tbody>
-      <tr v-for="data in dataset" :key="data.id">
-        <td v-for="head in columns" :key="head">{{data[head.key]}}</td>
+      <tr v-for="d in filteredData" :key="d.id">
+        <td v-for="head in columns" :key="head">{{d[head.key]}}</td>
       </tr>
     </tbody>
   </table>
+  <div class="page-change-section pull-left">
+    <div class="pull-left" v-if="currentEntry !== data.length">
+      <span class=" padding select-btn" @click="changePage('first')">&laquo;</span>
+      <span class=" padding select-btn" @click="changePage('previous')">&#8249;</span>
+      <span class=" padding text">Showing {{ currentPage }} of {{ pages }}</span>
+      <span class=" padding select-btn" @click="changePage('next')">&#8250;</span>
+      <span class=" padding select-btn" @click="changePage('last')">&raquo;</span>
+    </div>
+  </div>
+  <div class="select-entry-section pull-right">
+    <button
+      class="btn"
+      :class="{ selected: currentEntry === data.length }"
+      @click="updatePages(data.length)"
+    >
+      All
+    </button>
+    <button
+      class="btn"
+      v-for="(entry, i) in showEnteries"
+      :key="i"
+      :class="{ selected: currentEntry === entry }"
+      @click="updatePages(entry)"
+    >
+      {{ entry }}
+    </button>
+  </div>
 </template>
 <script>
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref, computed } from "vue";
 
 export default defineComponent({
   props: ["columns", "dataset"],
-  setup() {
+  setup(props) {
+    let data = ref(computed(() => props.dataset))
+    let filteredData = ref([]);
+    let currentPage = ref(1);
+    let currentEntry = ref(10);
+    let pages = ref(0);
+    let showEnteries = ref([10, 25, 50, 100]);
+
+    let pagination = () => {
+      let trimStart = (currentPage.value - 1) * currentEntry.value;
+      let trimEnd = trimStart + currentEntry.value;
+      console.log(trimStart, trimEnd)
+      console.log(data.value);
+      filteredData.value = data.value.slice(trimStart, trimEnd);
+      pages.value = Math.ceil(data.value.length / currentEntry.value);
+    };
+    
+    pagination();
+
+    let changePage = (page) => {
+      if (page === 'first') {
+        currentPage.value = 1;
+      } else if(page === 'previous') {
+        currentPage.value = currentPage.value === 1 ? currentPage.value : currentPage.value - 1; 
+      } else if (page === 'next') {
+        currentPage.value = currentPage.value === pages.value ? currentPage.value : currentPage.value + 1;
+      } else if (page === 'last') {
+        currentPage.value = pages.value;
+      }
+      pagination();
+    };
+
+    let updatePages = (entry) => {
+      currentEntry.value = entry;
+      currentPage.value = 1;
+      pagination();
+    };
+    return { 
+      filteredData,
+      changePage,
+      showEnteries,
+      currentPage,
+      updatePages,
+      currentEntry,
+      data,
+      pages
+     }
   }
 });
 </script>
